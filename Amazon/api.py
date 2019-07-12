2b73c71f0de1cbbbe17e19f9814229bc2036d894
sankalp7654 <sankalp.saxena.sta@gmail.com>
2019-07-12 15:20:29 +0530

    added sessions and login form

--- Amazon/api.py
+++ Amazon/api.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+#session is a special kind of dictionary that is accessible anywhere in the program
+#even in the jinja without passing it explicitly
 
 app = Flask(__name__)
+app.secret_key = 'hello'
 
 @app.route('/')
 def home():
@@ -14,4 +17,31 @@ def about():
 def contact():
 	return render_template('contact.html', title = "Contact")
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	
+	if request.method == 'POST':
+		users = {
+			'user1': '123',
+			'user2': '234', 
+			'user3': '456'
+		}
+
+		username = request.form['username']
+		password = request.form['password']
+
+		if username not in users:
+			return "User does not exist. Go back and enter a valid username"
+
+		if users[username] != password:
+			return "Password does not match. Go back and enter the password"
+
+		session['username'] = username
+		return redirect(url_for('home'))
+	return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+	session.clear()
+	return redirect(url_for('home'))
 app.run(debug = True)
